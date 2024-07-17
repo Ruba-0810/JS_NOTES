bf8630fd634a55b34e69af895581dbd3df4745a4
rubavathy0810@gmail.com <rubavathy0810@gmail.com>
2024-07-17 10:35:20 +0530

    add files

---
+++ class1/class.js
@@ -0,0 +1,24 @@
+//
+
+let sub= "Mathematics";
+console.log(sub.charAt(1));//
+console.log(sub.indexOf("at"));//1
+console.log(sub.indexOf("ematics"));//4
+console.log(sub.lastIndexOf("at"))//7
+console.log(sub.slice(2,5))// the
+console.log(sub.slice (9))//cs
+console.log(sub.toLowerCase().includes("math"));//true
+console.log(sub.split("Math"));
+console.log(sub.split(""));
+console.log(Math.PI);
+console.log(Math.trunc(7.4)); //7
+console.log(Math.floor(7.8));//7 it will take same value all times
+console.log(Math.ceil(7.4));//8 -it will take next value 
+console.log(Math.round(7.4));//8
+console.log(Math.round(7.4));//7
+console.log(Math.max(12,4,5,6));//12
+console.log(Math.min(1,2,3,4));//1
+console.log(Math.pow(2,4));//16
+console.log(Math.random())
+const n1= "jgihi423.098hjgkjhk";
+console.log(parseFloat(n1).toFixed(9));
